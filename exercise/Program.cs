f1012c4e8427d2efa0c16242472bf7784b06efad
app-arel <100846852+app-arel@users.noreply.github.com>
2024-03-11 07:53:27 +0100

    Updated Program.cs
    Added the comments

--- exercise/Program.cs
+++ exercise/Program.cs
@@ -1,4 +1,102 @@
-﻿// // class Program
+
+class Program{
+    //store the salary ranges in an array with their value set to 0
+    static int[] salaryRange = new int [9];
+    static void Main(string[] args){
+    int[] grossSales = [500, 1000, 1500, 2300, 10];
+    // you can input the gross sales by each customer in thks array
+    foreach (int pay in grossSales)
+    {
+        int salary = (int)(0.09 * pay + 200)-200/100;
+        //this is to calculate the ranges of the salary
+        Ranges(salary);
+    }
+        print();
+    }
+
+    static void Ranges(int salary){
+        //this method is to calculate the number of salaries that fall under the ranges of all the gross salaries in the gross salary array
+        if(salary>=200 && salary <300){
+            salaryRange[0]++;
+        }
+        else if(salary>=300 && salary<400){
+            salaryRange[1]++;
+        }
+        else if(salary>=400 && salary<500){
+            salaryRange[2]++;
+        }
+        else if(salary>=500 && salary<600){
+            salaryRange[3]++;
+        }
+        else if(salary>=600 && salary<700){
+            salaryRange[4]++;
+        }
+        else if(salary>=700 && salary<800){
+            salaryRange[5]++;
+        }
+        else if(salary>=800 && salary<900){
+            salaryRange[6]++;
+        }
+        else if(salary>=900 && salary<1000){
+            salaryRange[7]++;
+        }
+        else if(salary>=1000){
+            salaryRange[8]++;
+        }
+    }
+    static void print(){
+        //this method is to print the output so as to make your code less messy
+        Console.WriteLine("Ranges of salaries               Number of persons");
+        Console.WriteLine($"200 - $299                              {salaryRange[0]}");
+        Console.WriteLine($"300 - $399                              {salaryRange[1]}");
+        Console.WriteLine($"400 - $499                              {salaryRange[2]}");
+        Console.WriteLine($"500 - $599                              {salaryRange[3]}");
+        Console.WriteLine($"600 - $699                              {salaryRange[4]}");
+        Console.WriteLine($"700 - $799                              {salaryRange[5]}");
+        Console.WriteLine($"800 - $899                              {salaryRange[6]}");
+        Console.WriteLine($"900 - $999                              {salaryRange[7]}");
+        Console.WriteLine($"1000                                    {salaryRange[8]}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // class Program
 // // {
 // //     static bool[] seatClass = new bool[10];
 // //     static void Main(string[] args)
@@ -93,117 +191,4 @@
 // //         }
 // //     }
 // // }
-// class Program{
 
-// static int[] rangeOfSalaries = new int[9];
-//     static void Main(string[]args){
-// int[] grossSales = [2300, 400, 1242, 780, 560, 430, 3400,12000, 334, 992, 8893];
-
-//  int count = 0;
-// foreach(int sale in grossSales){
-//     double salary = (0.09 * sale) + 200;
-//     RangeDisplay(salary);
-// }
-// Print();
-// }
-
-
-// static void RangeDisplay(double salary)
-// {
-//     if(salary>200 && salary < 299){
-//         rangeOfSalaries[0]++;
-//     }
-//     else if(salary>300 && salary < 399){
-//         rangeOfSalaries[1]++;
-//     }
-//     else if(salary>400 && salary < 499){
-//         rangeOfSalaries[2]++;
-//     }
-//     else if(salary>500 && salary < 599){
-//         rangeOfSalaries[3]++;
-//     }
-//     else if(salary>600 && salary < 699){
-//         rangeOfSalaries[4]++;
-//     }
-//     else if(salary>700 && salary < 799){
-//         rangeOfSalaries[5]++;
-//     }
-//     else if(salary>800 && salary < 899){
-//         rangeOfSalaries[6]++;
-//     }
-//     else if(salary>900 && salary < 999){
-//         rangeOfSalaries[7]++;
-//     }
-//     else if(salary>1000){
-//         rangeOfSalaries[8]++;
-//     }
-// }
-//     static void Print(){
-//         Console.WriteLine("Range of salaries            Number of customers in category");
-//         Console.WriteLine($"$200-$299                              {rangeOfSalaries[0]}");
-//         Console.WriteLine($"$300-$399                              {rangeOfSalaries[1]}");
-//         Console.WriteLine($"$400-$499                              {rangeOfSalaries[2]}");
-//         Console.WriteLine($"$500-$599                              {rangeOfSalaries[3]}");
-//         Console.WriteLine($"$600-$699                              {rangeOfSalaries[4]}");
-//         Console.WriteLine($"$700-$799                              {rangeOfSalaries[5]}");
-//         Console.WriteLine($"$800-$899                              {rangeOfSalaries[6]}");
-//         Console.WriteLine($"$900-$999                              {rangeOfSalaries[7]}");
-//         Console.WriteLine($"$1000 and above                        {rangeOfSalaries[8]}");
-//     }
-// }
-
-class Program{
-    static int[] salaryRange = new int [9];
-    static void Main(string[] args){
-    int[] grossSales = [500, 1000, 1500, 2300, 10];
-
-    foreach (int pay in grossSales)
-    {
-        int salary = (int)(0.09 * pay + 200)-200/100;
-        Ranges(salary);
-    }
-        print();
-    }
-
-    static void Ranges(int salary){
-        if(salary>=200 && salary <300){
-            salaryRange[0]++;
-        }
-        else if(salary>=300 && salary<400){
-            salaryRange[1]++;
-        }
-        else if(salary>=400 && salary<500){
-            salaryRange[2]++;
-        }
-        else if(salary>=500 && salary<600){
-            salaryRange[3]++;
-        }
-        else if(salary>=600 && salary<700){
-            salaryRange[4]++;
-        }
-        else if(salary>=700 && salary<800){
-            salaryRange[5]++;
-        }
-        else if(salary>=800 && salary<900){
-            salaryRange[6]++;
-        }
-        else if(salary>=900 && salary<1000){
-            salaryRange[7]++;
-        }
-        else if(salary>=1000){
-            salaryRange[8]++;
-        }
-    }
-    static void print(){
-        Console.WriteLine("Ranges of salaries               Number of persons");
-        Console.WriteLine($"200 - $299                              {salaryRange[0]}");
-        Console.WriteLine($"300 - $399                              {salaryRange[1]}");
-        Console.WriteLine($"400 - $499                              {salaryRange[2]}");
-        Console.WriteLine($"500 - $599                              {salaryRange[3]}");
-        Console.WriteLine($"600 - $699                              {salaryRange[4]}");
-        Console.WriteLine($"700 - $799                              {salaryRange[5]}");
-        Console.WriteLine($"800 - $899                              {salaryRange[6]}");
-        Console.WriteLine($"900 - $999                              {salaryRange[7]}");
-        Console.WriteLine($"1000                                    {salaryRange[8]}");
-    }
-}
